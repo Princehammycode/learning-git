47e85913456a350ceadf6756d80ac15d409846d5
Princehammycode <108911841+Princehammycode@users.noreply.github.com>
2023-04-15 11:38:21 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 Today is Weekend
 It is another Weekend DevOps study day.
 I am very glad
+<p>
+This paragraph
+contains a lot of lines
+in the source code,
+but the browser
+ignores it.
+</p>
+
+<p>
+This paragraph
+contains         a lot of spaces
+in the source         code,
+but the        browser
+ignores it.
+</p>
