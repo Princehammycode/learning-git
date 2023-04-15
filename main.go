fbe0d99934dbc297cb1271d930b4aa65ec77e6ca
Princehammycode <princehammond2579@gmail.com>
2023-04-15 10:46:47 +0100

    Modified main.go file adding HTML code

--- main.go
+++ main.go
@@ -16,3 +16,15 @@ in the source         code,
 but the        browser
 ignores it.
 </p>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
